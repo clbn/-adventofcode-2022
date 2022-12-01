8099164a8ef77e051bbb86ab323d44f3174506ef
Alex Olshansky <alex@creagenics.com>
2022-12-01 21:35:04 +0100

    Day 1: Calorie Counting (part 2)

--- day-1/calorie-counting.js
+++ day-1/calorie-counting.js
@@ -4,6 +4,15 @@ const elves = data.trim().split(/\r?\n\r?\n/)
   .map(elf => elf.split(/\r?\n/)
     .reduce((acc, cal) => acc + +cal, 0));
 
+// --- Part One ---
+
 const topLoad = Math.max(...elves);
 
 console.log(topLoad);
+
+// --- Part Two ---
+
+const sortedElves = elves.sort((a, b) => a - b);
+const topThreeLoad = sortedElves.slice(-3).reduce((acc, cal) => acc + cal, 0);
+
+console.log(topThreeLoad);
