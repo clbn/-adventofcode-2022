0b52961830f60b6e1599ae0c50ce4aeb55a539c4
Alex Olshansky <alex@creagenics.com>
2022-12-03 12:02:02 +0100

    Day 3: Rucksack Reorganization (part 2)

--- day-3/rucksack-reorganization.js
+++ day-3/rucksack-reorganization.js
@@ -4,6 +4,8 @@ const rucksacks = data.trim().split(/\r?\n/);
 
 const abc = '_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
+// --- Part One ---
+
 const errors = rucksacks
   .map(r => {
     const h1 = r.slice(0, r.length / 2).split('');
@@ -13,3 +15,21 @@ const errors = rucksacks
   .reduce((acc, l) => acc + abc.indexOf(l), 0);
 
 console.log(errors);
+
+// --- Part Two ---
+
+const groups = [];
+for (let i = 0; i < rucksacks.length; i += 3) {
+  groups.push(rucksacks.slice(i, i + 3));
+}
+
+const badges = groups
+  .map(r => {
+    const r1 = r[0].split('');
+    const r2 = r[1].split('');
+    const r3 = r[2].split('');
+    return r1.filter(l => r2.includes(l)).filter(l => r3.includes(l))[0];
+  })
+  .reduce((acc, l) => acc + abc.indexOf(l), 0);
+
+console.log(badges);
