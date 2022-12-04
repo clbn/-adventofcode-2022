548bc61e22cba5ab34cf697b8639407abd075477
Alex Olshansky <alex@creagenics.com>
2022-12-04 11:43:57 +0100

    Day 4: Camp Cleanup (part 2)

--- day-4/camp-cleanup.js
+++ day-4/camp-cleanup.js
@@ -4,9 +4,20 @@ const sections = data.trim().split(/\r?\n/)
   .map(line => line.split(/[,-]/)
     .map(n => +n));
 
+// --- Part One ---
+
 const fullyContained = sections.filter(([a, b, c, d]) =>
   (a >= c && b <= d) ||
   (a <= c && b >= d)
 );
 
 console.log(fullyContained.length);
+
+// --- Part Two ---
+
+const overlapped = sections.filter(([a, b, c, d]) =>
+  (a <= c && b >= c) ||
+  (a >= c && a <= d)
+);
+
+console.log(overlapped.length);
