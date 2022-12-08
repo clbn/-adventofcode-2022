d314f7b8e80d505f09b1bf55fa58816544ef8b00
Alex Olshansky <alex@creagenics.com>
2022-12-08 15:24:42 +0100

    Day 6: Tuning Trouble (part 2)

--- day-6/tuning-trouble.js
+++ day-6/tuning-trouble.js
@@ -1,17 +1,27 @@
 const fs = require('fs');
 const data = fs.readFileSync('input.txt', { encoding: 'utf8' });
 
-const seq = [];
-let i;
+const getSequenceIndex = (n) => {
+  const seq = [];
+  let i;
 
-for (i = 0; i < data.length; i++) {
-  if (seq.length === 4) {
-    if ((new Set(seq)).size === seq.length) {
-      break;
+  for (i = 0; i < data.length; i++) {
+    if (seq.length === n) {
+      if ((new Set(seq)).size === seq.length) {
+        break;
+      }
+      seq.shift();
     }
-    seq.shift();
+    seq.push(data[i]);
   }
-  seq.push(data[i]);
+
+  return i;
 }
 
-console.log(i);
+// --- Part One ---
+
+console.log(getSequenceIndex(4));
+
+// --- Part Two ---
+
+console.log(getSequenceIndex(14));
