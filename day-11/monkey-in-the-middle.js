655c0a56beebd767f964c1f9a6714fdd694db972
Alex Olshansky <alex@creagenics.com>
2022-12-11 16:55:14 +0100

    Day 11: Monkey in the Middle (part 2)

--- day-11/monkey-in-the-middle.js
+++ day-11/monkey-in-the-middle.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const data = fs.readFileSync('input.txt', { encoding: 'utf8' });
-const monkeys = data.trim().split(/\r?\n\r?\n/).map(m => {
+const initialState = data.trim().split(/\r?\n\r?\n/).map(m => {
   const lines = m.split(/\r?\n/);
   return {
     items: [...lines[1].match(/\d+/g)].map(n => +n),
@@ -12,23 +12,33 @@ const monkeys = data.trim().split(/\r?\n\r?\n/).map(m => {
   };
 });
 
-let rounds = 20;
-while (rounds--) {
-  monkeys.forEach(m => {
-    m.items.forEach(old => {
-      // `old` is inside eval
-      const afterOp = Math.floor(eval(m.operation) / 3);
-      const destination = afterOp % m.test ? m.no : m.yes;
-      monkeys[destination].items.push(afterOp);
-      m.business++;
-    })
-    m.items = [];
-  });
-}
+const getLevel = (rounds, divisor) => {
+  const monkeys = JSON.parse(JSON.stringify(initialState));
+  const inhibitor = monkeys.map(m => m.test).reduce((acc, b) => acc * b);
 
-const level = monkeys.map(m => m.business)
-  .sort((a, b) => a - b)
-  .slice(-2)
-  .reduce((acc, b) => acc * b)
+  while (rounds--) {
+    monkeys.forEach(m => {
+      m.items.forEach(old => {
+        // `old` is inside eval
+        const afterOp = Math.floor(eval(m.operation) / divisor);
+        const destination = afterOp % m.test ? m.no : m.yes;
+        monkeys[destination].items.push(afterOp % inhibitor);
+        m.business++;
+      })
+      m.items = [];
+    });
+  }
 
-console.log(level);
+  return monkeys.map(m => m.business)
+    .sort((a, b) => a - b)
+    .slice(-2)
+    .reduce((acc, b) => acc * b)
+};
+
+// --- Part One ---
+
+console.log(getLevel(20, 3));
+
+// --- Part Two ---
+
+console.log(getLevel(10000, 1));
