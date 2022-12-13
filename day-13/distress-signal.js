b04ba80a76a6067120b50bf2eb98a0ea2e2504e7
Alex Olshansky <alex@creagenics.com>
2022-12-13 11:22:13 +0100

    Day 13: Distress Signal (part 1)

---
+++ day-13/distress-signal.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const data = fs.readFileSync('input.txt', { encoding: 'utf8' });
+const pairs = data.trim().split(/\r?\n\r?\n/)
+  .map(pair => pair.split(/\r?\n/)
+    .map(packet => JSON.parse(packet)));
+
+const compare = (a, b) => {
+  if (Number.isInteger(a) && Number.isInteger(b)) {
+    return a < b ? -1 : a > b ? 1 : 0;
+  }
+
+  if (Number.isInteger(a)) {
+    return compare([a], b);
+  }
+
+  if (Number.isInteger(b)) {
+    return compare(a, [b]);
+  }
+
+  for (let i = 0; i < Math.max(a.length, b.length); i++) {
+    if (a[i] === undefined) return -1;
+    if (b[i] === undefined) return 1;
+    const c = compare(a[i], b[i]);
+    if (c !== 0) {
+      return c;
+    }
+  }
+
+  return 0;
+};
+
+const goodOnes = pairs
+  .map(([a, b]) => compare(a, b) < 1)
+  .reduce((acc, v, i) => v ? acc + i + 1 : acc, 0);
+
+console.log(goodOnes);
