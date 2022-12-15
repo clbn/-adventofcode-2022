2bfb47bbbe0fe9af78e86a4880dd66f81a246007
Alex Olshansky <alex@creagenics.com>
2022-12-15 20:22:15 +0100

    Day 15: Beacon Exclusion Zone (part 2)

--- day-15/beacon-exclusion-zone.js
+++ day-15/beacon-exclusion-zone.js
@@ -5,6 +5,8 @@ const lines = data.trim().split(/\r?\n/)
   .map(line => [...line.match(/-?\d+/g)]
     .map(n => +n));
 
+// --- Part One ---
+
 const distance = (ax, ay, bx, by) => Math.abs(ax - bx) + Math.abs(ay - by);
 
 const xx = new Set();
@@ -24,3 +26,50 @@ for (const [sx, sy, bx, by] of lines) {
 }
 
 console.log(xx.size);
+
+// --- Part Two ---
+
+const joinRangesOrDiscoverGap = ([a, b], [c, d]) => {
+  if (b + 1 < c) return b + 1;
+  if (d + 1 < a) return d + 1;
+  return [Math.min(a, c), Math.max(b, d)];
+};
+
+const clearOneLine = (y) => {
+  const ranges = [];
+
+  for (const [sx, sy, bx, by] of lines) {
+    const can = distance(sx, sy, bx, by);
+    const need = distance(0, sy, 0, y);
+    const touch = can - need;
+    if (touch >= 0) {
+      ranges.push([sx - touch, sx + touch]);
+    }
+  }
+
+  ranges.sort((a, b) => a[0] - b[0]);
+
+  while (ranges.length > 1) {
+    const r1 = ranges.shift();
+    const r2 = ranges.shift();
+    const rangeOrGap = joinRangesOrDiscoverGap(r1, r2);
+
+    if (Number.isInteger(rangeOrGap)) {
+      return rangeOrGap; // found the gap
+    }
+
+    ranges.unshift(rangeOrGap);
+  }
+
+  return null;
+};
+
+let gapx = 0;
+let gapy = 4000000;
+
+while (gapy--) {
+  gapx = clearOneLine(gapy);
+  if (gapx !== null) break;
+}
+
+console.log(gapx * 4000000 + gapy);
