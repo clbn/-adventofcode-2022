0ccc055e47b9e6b134f885a5b8d1070f0d7786e4
Alex Olshansky <alex@creagenics.com>
2022-12-19 00:56:19 +0100

    Day 18: Boiling Boulders (part 2)

--- day-18/boiling-boulders.js
+++ day-18/boiling-boulders.js
@@ -6,9 +6,51 @@ const droplets = lines.map(row => row.split(',').map(n => +n));
 const lava = new Set(lines);
 const directions = [[-1, 0, 0], [1, 0, 0], [0, -1, 0], [0, 1, 0], [0, 0, -1], [0, 0, 1]];
 
+// --- Part One ---
+
 const surface = droplets.reduce((acc, [x, y, z]) => {
   const neighbors = directions.filter(([dx, dy, dz]) => lava.has(`${x+dx},${y+dy},${z+dz}`))
   return acc - neighbors.length;
 }, droplets.length * 6);
 
 console.log(surface);
+
+// --- Part Two ---
+
+const [min, max] = droplets.reduce(
+  ([[xmin, ymin, zmin], [xmax, ymax, zmax]], [x, y, z]) => [
+    [Math.min(xmin, x-1), Math.min(ymin, y-1), Math.min(zmin, z-1)],
+    [Math.max(xmax, x+1), Math.max(ymax, y+1), Math.max(zmax, z+1)],
+  ],
+  [[Infinity, Infinity, Infinity], [-Infinity, -Infinity, -Infinity]]
+);
+const [xmin, ymin, zmin] = min;
+const [xmax, ymax, zmax] = max;
+
+const air = new Set();
+air.add(min.join(','));
+
+const explore = ([x, y, z]) =>
+  (x >= xmin && y >= ymin && z >= zmin) &&
+  (x <= xmax && y <= ymax && z <= zmax) &&
+  !lava.has(`${x},${y},${z}`) &&
+  !air.has(`${x},${y},${z}`);
+
+const queue = [min];
+while (queue.length > 0) {
+  const [cx, cy, cz] = queue.shift();
+  for (const [dx, dy, dz] of directions) {
+    const next = [cx + dx, cy + dy, cz + dz];
+    if (explore(next)) {
+      queue.push(next);
+      air.add(next.join(','));
+    }
+  }
+}
+
+const exterior = droplets.reduce((acc, [x, y, z]) => {
+  const neighbors = directions.filter(([dx, dy, dz]) => !air.has(`${x+dx},${y+dy},${z+dz}`))
+  return acc - neighbors.length;
+}, droplets.length * 6);
+
+console.log(exterior);
